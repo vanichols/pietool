a8dc03019ba98aa480bc29a7722e8c66946e5674
vanichols <virginia.nichols@gmail.com>
2026-03-29 16:02:58 +0200

    dont have pub fig code in r folder

--- R/pub_figs.R
+++ R/pub_figs.R
@@ -1,15 +1,15 @@
 #--make figs for pub
 
-library(tidyverse)
-library(scales)
-library(patchwork)
-
-rm(list = ls())
-
-source("R/utils.R")
-
-# data --------------------------------------------------------------------
-
-fxn_Make_Costs_Plot2()
-ggsave("www/costs-ex-fig.png", 
-       width = 4, height = 6)  
+# library(tidyverse)
+# library(scales)
+# library(patchwork)
+# 
+# rm(list = ls())
+# 
+# source("R/utils.R")
+# 
+# # data --------------------------------------------------------------------
+# 
+# fxn_Make_Costs_Plot2()
+# ggsave("www/costs-ex-fig.png", 
+#        width = 4, height = 6)  
